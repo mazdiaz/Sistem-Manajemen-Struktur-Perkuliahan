21f4ed76675a6c6f9c78d248e6677ab646db0bf6
mazdiaz <46370875+mazdiaz@users.noreply.github.com>
2024-12-17 15:35:54 +0700

    Create main.cpp
    Struktur MLL

---
+++ Main Program/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using std::string;
+
+struct Mahasiswa {
+    string nama;
+    string NIM;
+    Mahasiswa* next;
+
+    Mahasiswa() : next(nullptr) {}
+};
+
+struct Kelas {
+    string id_Kelas;
+    Kelas* next;
+    Mahasiswa* head_mahasiswa;
+
+    Kelas() {
+        next = nullptr;
+        head_mahasiswa = nullptr;
+    }
+};
+
+struct Jurusan {
+    string nama_jurusan;
+    Jurusan* next;
+    Mahasiswa* head_mahasiswa; 
+
+    Jurusan() {
+        next = nullptr;
+        head_mahasiswa = nullptr;
+    }
+};
+
+struct Fakultas {
+    string nama_fakultas;
+    Fakultas* next;
+    Jurusan* head_jurusan;
+
+    Fakultas() {
+        next = nullptr;
+        head_jurusan = nullptr;
+    }
+};
+
+int main() {
+
+
+
+    return 0;
+}
